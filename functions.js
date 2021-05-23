5cb824e1cbbcb1143a06aa4be5b3f16128886208
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-23 16:49:17 -0500

    :art: Update

---
+++ functions.js
@@ -0,0 +1,11 @@
+const username = 'Saitama';
+const userAge = 55;
+
+function printAge(name, age) {
+  console.log(`${name} is ${age} years old.`);
+}
+
+// Arow function:
+// const printAge = (name,age) => console.log(`${name} is ${age} years old.`)
+
+printAge(username, userAge);
---
+++ scoop.js
@@ -0,0 +1,10 @@
+const myName = 'UltiRequiem';
+
+function printinUppercase(n) {
+  // console.log(myName.toUpperCase())
+  console.log(n.toUpperCase());
+}
+
+// const printinUppercase = n => console.log(n.toUpperCase())
+
+printinUppercase(myName);
