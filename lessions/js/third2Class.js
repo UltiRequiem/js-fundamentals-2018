9fb9ad514cda5c6e91f9b2c7c39005fce05a85a5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-23 11:50:29 -0500

    :sparkles: Update

---
+++ lessions/js/third2Class.js
@@ -0,0 +1,8 @@
+const cost = 200.3;
+
+const total = (Math.round(cost * 100 * 3) / 100).toFixed(3);
+
+const totalFloat = parseFloat(total);
+
+console.log(total);
+console.log(totalFloat);
---
+++ lessions/js/third3Class.js
@@ -0,0 +1,6 @@
+const pizzaPieces = 8;
+const persons = 2;
+
+const piecesPerPerson = pizzaPieces / persons;
+
+console.log(`Each person has to eat ${piecesPerPerson} pieces of Pizza.`);
