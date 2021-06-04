0cbe1ff09e790628bf8a671223ff8f96068345b7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-04 11:47:15 -0500

    :art: Update

---
+++ 03-arrays/19-transformAnArray.js
@@ -0,0 +1,38 @@
+const martin = {
+  nombre: 'Martin',
+  apellido: 'Gomez',
+  altura: 1.85,
+};
+
+const dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.71,
+};
+
+const vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56,
+};
+
+const paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76,
+};
+
+const esAlta = ({ altura }) => altura > 1.8;
+
+const persons = [martin, dario, vicky, paula];
+
+const personasAltas = persons.filter(esAlta);
+
+const pasarAlturaACms = (persona) => ({
+  ...persona,
+  altura: persona.altura * 100,
+});
+
+const personasCms = persons.map(pasarAlturaACms);
+
+console.log(personasCms);
