73f8584da0a2fbed2dc0da1154f69c92b9ad02e6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-04 10:56:23 -0500

    :art: Update

---
+++ others/arrayFilter.js
@@ -0,0 +1,6 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+// Return number if is bigger than 5
+const testFunction = (number) => number > 5;
+// Recibe a function
+const arrayCopy = numbers.filter(testFunction);
+console.log(arrayCopy);
