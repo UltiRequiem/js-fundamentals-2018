cd532e32460db0b1e5d68121f47ba4d0a92939aa
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-09 18:58:12 -0500

    :art: Update

--- others/yoSoyTuPadre.py
+++ others/yoSoyTuPadre.py
@@ -1,15 +1,11 @@
 # ./yoSoyTuPadre.js en Python
 import requests
 
-API_URL = "https://swapi.dev/api/"
-PEOPLE_URL = "people/n"
+API_URL = "https://swapi.dev/api/people/n".replace("n", "5")
 
-PERSON = f"{API_URL}{PEOPLE_URL.replace('n','4')}"
+DATA = requests.get(API_URL).json()
 
-r = requests.get(PERSON)
-data = r.json()
-
-if data["name"] == "Darth Vader":
+if DATA["name"] == "Darth Vader":
     print("Yo soy tu padre.")
 else:
-    print(f'Me llamo {data["name"]}.')
+    print(f"Me llamo {DATA['name']}.")
