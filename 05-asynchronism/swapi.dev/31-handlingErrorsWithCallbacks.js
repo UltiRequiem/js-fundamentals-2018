7255b71dd82e3570bdb6a7439a642cddc55a9886
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-09 14:07:20 -0500

    :art: Update
    Pal icpna

---
+++ 05-asynchronism/swapi.dev/31-handlingErrorsWithCallbacks.js
@@ -0,0 +1,44 @@
+/*
+ *  Para solucionar el problema de quedarnos sin conexión, u otro error similar,
+ *  en medio de una sucesión de callbacks utilizamos el método fail().
+ */
+
+const API_URL = 'https://swapi.co/api/';
+const PEOPLE_URL = 'people/:id';
+const opts = { crossDomain: true };
+
+function obtenerPersonaje(id, callback) {
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+
+  $.get(url, opts, callback).fail(() => {
+    console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`);
+  });
+}
+
+obtenerPersonaje(1, (personaje) => {
+  console.log(`Hola, yo soy ${personaje.name}`);
+
+  obtenerPersonaje(2, (personaje) => {
+    console.log(`Hola, yo soy ${personaje.name}`);
+
+    obtenerPersonaje(3, (personaje) => {
+      console.log(`Hola, yo soy ${personaje.name}`);
+
+      obtenerPersonaje(4, (personaje) => {
+        console.log(`Hola, yo soy ${personaje.name}`);
+
+        obtenerPersonaje(5, (personaje) => {
+          console.log(`Hola, yo soy ${personaje.name}`);
+
+          obtenerPersonaje(6, (personaje) => {
+            console.log(`Hola, yo soy ${personaje.name}`);
+
+            obtenerPersonaje(7, (personaje) => {
+              console.log(`Hola, yo soy ${personaje.name}`);
+            });
+          });
+        });
+      });
+    });
+  });
+});
