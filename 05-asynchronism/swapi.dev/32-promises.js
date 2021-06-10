66956d5570c49f816ebeb5ae478643a7735d2b6c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-10 06:28:59 -0500

    :art: Update

--- 05-asynchronism/swapi.dev/32-promises.js
+++ 05-asynchronism/swapi.dev/32-promises.js
@@ -4,21 +4,20 @@ const request = require('request');
 
 const API_URL = 'https://swapi.dev/api/people';
 
-function getPJ(id) {
-  return new Promise((resolve, reject) => {
+const getPJ = (id) =>
+  new Promise((resolve, reject) => {
     request(`${API_URL}/${id}`, (error, _response, body) => {
       if (error) reject(error);
       else resolve(body);
     });
   });
-}
 
-function getName(data) {
+const getName = (data) => {
   const { name } = JSON.parse(data);
   return name;
-}
+};
 
-for (let i = 1; i < 15; i++) {
+for (let i = 1; i <= 15; i++) {
   getPJ(i).then((data) => {
     console.log(`My name is ${getName(data)}.`);
   });
