5fe03dbc4a4b8519c505d20bd4f6022654d2d7f6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-10 10:45:17 -0500

    :art: Update

---
+++ others/async/errorHandlingTwo.js
@@ -0,0 +1,16 @@
+const caserUpper = (val) =>
+  new Promise((resolve, reject) => {
+    resolve(val.toUpperCase());
+  });
+
+const msg = async (x) => {
+  try {
+    const message = await caserUpper(x);
+    console.log(message);
+  } catch (err) {
+    console.log(`Ohh no... ${err.message} :(`);
+  }
+};
+
+msg('JavaScript is as Nice as Neovim.');
+msg(34); // Ohh no: val.toUpperCase is not a function
