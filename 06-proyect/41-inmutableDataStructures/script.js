26d0b08710843782e55ecd697c52c06d896cefbb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-10 14:15:28 -0500

    :art: Update

---
+++ 06-proyect/41-inmutableDataStructures/script.js
@@ -0,0 +1,14 @@
+const sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  edad: 28,
+};
+
+const cumpleanosInmutable = (persona) => ({
+  ...persona,
+  edad: persona.edad + 1,
+});
+
+const sachaViejo = cumpleanosInmutable(sacha);
+console.log(sacha);
+console.log(sachaViejo);
